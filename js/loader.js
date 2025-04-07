52669eb3b5d36a0deb5ea2ba183097e627c131b3
insect2510 <155474669+insect2510@users.noreply.github.com>
2025-04-08 01:37:55 +0200

    Fix: subpage-loader in loader.js integriert
    - Frage Klasse subpage im tag main ab

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,11 @@
-$("header").load("components/header.html");
-$("footer").load("components/footer.html");
-$("#script").load("js/script.js");
+$(document).ready(function () {
+    if ($("main").hasClass("subpage")) {
+        $("header").load("../components/subpage-header.html");
+        $("footer").load("../components/subpage-footer.html");
+        $("#script").load("../js/script.js");
+    } else {
+        $("header").load("components/header.html");
+        $("footer").load("components/footer.html");
+        $("#script").load("js/script.js");
+    }
+});
--- js/subpage-loader.js
+++
@@ -1,3 +0,0 @@
-$("header").load("../components/subpage-header.html");
-$("footer").load("../components/subpage-footer.html");
-$("#script").load("../js/script.js");
